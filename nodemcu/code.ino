504480f6a95c8af4a1dedd244c1275a4ff4aacaf
AKHIL E <102219762+ak10101@users.noreply.github.com>
2022-05-24 18:21:22 +0530

    Update code.ino

--- nodemcu/code.ino
+++ nodemcu/code.ino
@@ -4,13 +4,25 @@
 #include <ArduinoJson.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial display(2, 3); 
+#define D_B = D2;
+/*
+Welcome Screen: 1
+Order OK: 2
+QR Ready: 3
+Order Fail: 4
+Order Retrying: 5
+TXN Failed: 6
+Txn Success: 7
+Dispensing: 8
+Thankyou: 9
 
-const char * ssid = "AKHIL";
-const char * password = "123456789";
+*/
 
-//Your Domain name with URL path or IP address with path
-const char * serverName = "https://api.cornercafe.shop/tea/new";
+SoftwareSerial display(D2, D3); 
+
+const char * ssid = "iPhone";
+const char * password = "12345678";
+const char * serverName = "https://api.cornercafe.shop";
 
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
@@ -19,9 +31,11 @@ StaticJsonDocument < 130 > inputData;
 JsonObject config = inputData.createNestedObject("config");
 StaticJsonDocument<6000> doc;
 
+String req_id;
+
 void setup() {
   Serial.begin(115200);
-  display.begin(115200);
+  display.begin(19200);
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -31,9 +45,14 @@ void setup() {
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
-
+  pinMode(D_B, INPUT);
 }
 
+void sendToDisplay(const char * qrdata){
+//  display.print('\n');
+  display.print(qrdata);
+  display.print('\n');
+  }
 
 void sendQR(const char * qrData, int width){
   String data = String(qrData);
@@ -41,60 +60,107 @@ void sendQR(const char * qrData, int width){
   Serial.println(data);
   const int len = data.length();
   Serial.println(len);
-  display.print('WIDTH: ');
-  display.print(width);
+  display.print("WIDTH: ");
+  display.print(width);display.print('\n');
   int i,j = 0;
   unsigned int count = 0;
 for(j=0; j<width; j++){
   for(i=0; i<width; i++){
    temp = data.charAt(i+(j*width));
-       display.print('QR');
+       display.print("QR");
        display.print('X');
        display.print(i);
        display.print('Y');
        display.print(j);
        display.print('D');
        display.print(temp);
+       display.print('\n');
+       delay(2);
+       
   }
 }  
 }
 
+void sendCommand(int cmd){
+  display.print("CMD");
+  display.print(String(cmd));
+  display.print('\n');
+  }
+
+void request_and_send_qr_to_display(){
+   WiFiClientSecure client;
+      client.setInsecure();
+      HTTPClient http;
+      String httpRequestData;
+      inputData["count"] = 1;
+      inputData["machine_number"] = "C21";
+      inputData["amount"] = 10;
+      inputData["type"] = "tea";
+      config["water_level"] = 60;
+      config["tea_powder"] = 70;
+      config["coffee_power"] = 80;
+      serializeJson(inputData, httpRequestData);
+      Serial.println("INPUT: " + httpRequestData);
+      http.begin(client, serverName + "/tea/new");
+      http.addHeader("Content-Type", "application/json");
+      int httpResponseCode = http.POST(httpRequestData);
+      Serial.println(httpResponseCode);
+      while (httpResponseCode == 200){
+        int httpResponseCode = http.POST(httpRequestData);
+        }
+      DeserializationError error = deserializeJson(doc, http.getString());
+      http.end();
+      if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+      }
+      int width = doc["size"];
+      req_id = doc["request_id"];
+      sendQR(doc["qr"], width);
+      workflow_status = 1;
+  }
 
 
-void displayTxnSuccess(){
-  display.print('TXN SUCC');
-}
+void check_txn_status(){
+   WiFiClientSecure client;
+      client.setInsecure();
+      HTTPClient http;
+      String link = serverName + "/payment/status/"
+       http.begin(client, link + req_id);
+       int httpResponseCode = http.GET();
+       if (httpResponseCode != 200){
+         sendCommand(4);
+         return;
+        }
+        // Continue from here.
+  }
 
-void displayTxnFailed(){
-  display.print('TXN FAIL');
-}
 
-void displayNewOrder(){
-  display.print('NEW TEA ');
-}
 
-void displayOrderReady(){
-  display.print('ORDER OK');
-}
-
-void displayThanks(){
-  display.print('ORDER DN');
-}
-
-void displayWelcome(){
-  display.print('WELCOME ');
-}
+unsigned long qr_req_time;
+int workflow_status = 0;
+boolean swith = false;
 
 
 void loop() {
   //Send an HTTP POST request every 10 minutes
+  digitalRead(D_B) ? swith = true : swith = false;
+  
+  if (workflow_status == 0){
+     swith ? request_and_send_qr_to_display() : sendCommand(1);
+     return; 
+    }
+   if (workflow_status == 1){
+    check_txn_status();
+    }
+       
   if ((millis() - lastTime) > timerDelay) {
     //Check WiFi connection status
     if (WiFi.status() == WL_CONNECTED) {
       WiFiClientSecure client;
       client.setInsecure();
       HTTPClient http;
-
       String httpRequestData;
       inputData["count"] = 2;
       inputData["machine_number"] = "C21";
@@ -104,23 +170,20 @@ void loop() {
       config["tea_powder"] = 70;
       config["coffee_power"] = 80;
       serializeJson(inputData, httpRequestData);
-
+      Serial.println("INPUT: " + httpRequestData);
       http.begin(client, serverName);
       http.addHeader("Content-Type", "application/json");
       int httpResponseCode = http.POST(httpRequestData);
-      String response = http.getString();
+      Serial.println(httpResponseCode);
+      DeserializationError error = deserializeJson(doc, http.getString());
       http.end();
-
-      DeserializationError error = deserializeJson(doc, response);
       if (error) {
         Serial.print(F("deserializeJson() failed: "));
         Serial.println(error.f_str());
         return;
       }
-      const char * qr = doc["qr"];
       int width = doc["size"];
-      sendQR(qr, width);
-
+      sendQR(doc["qr"], width);
     } else {
       Serial.println("WiFi Disconnected");
     }
